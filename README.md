34b8a35f2643b880d94923688ecd4fb10d3920c7
IngenieroAlan <brandon.rod182@gmail.com>
2024-09-05 14:10:57 -0700

    Fixing wrong import and adding readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Monani App
+Proyecto que será utilizado para ayudar a los ganaderos con su ganado vacuno
+
+## Índice
+- [Instalación](#id1)
+- [Autores](#id2)
+
+## Instalación
+
+## Autores
+
+- Brandon Rodríguez -> [@ingenieroAlan](https://www.github.com/ingenieroAlan)
+- Juan Angulo -> [@AnguloJuan](https://github.com/AnguloJuan)
+- Marielena Nuñez -> [@marielena08](https://github.com/marielena08)
+- Miguel Dominguez -> [@IvaanMiguel](https://github.com/IvaanMiguel)
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
-import { PaperModal } from "../components/paperModal";
+import { PaperModal } from "../components/PaperModal";
 export const HomeView = () => {
   return (
     <View style={styles.container}>
-      <Text variant="titleLarge" theme={{colors:{primary:'black'}}} style={{color:'#000'}}>Hola papu!</Text>
+      <Text variant="titleLarge" style={{color:'#000'}}>Hola papu!</Text>
       <PaperModal content="Hola usuario" btnText="Presioname" />
       <StatusBar style="auto" />
     </View>

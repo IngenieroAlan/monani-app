6dd016c7da49795c8a9cf2e0a4d7753dcb0d799d
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-10-15 02:17:26 -0700

    FilterChip props updated.

--- src/components/FilterChip.tsx
+++ src/components/FilterChip.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Chip, ChipProps, useTheme } from 'react-native-paper'
 
-const FilterChip = <T,>(props: ChipProps & { filters: Set<T> }) => {
+const FilterChip = <T,>(props: ChipProps & { filters: T[] }) => {
   const theme = useTheme()
   const [filters, setFilters] = useState(props.filters)
 
@@ -11,18 +11,18 @@ const FilterChip = <T,>(props: ChipProps & { filters: Set<T> }) => {
 
   const getFilterText = () => {
     const firstFilter = [...filters][0]
-    const plus = filters.size - 1 === 0 ? '' : ` +${filters.size - 1}`
+    const plus = filters.length - 1 === 0 ? '' : ` +${filters.length - 1}`
 
     return `${firstFilter}${plus}`
   }
 
   return (
     <Chip
-      style={{ backgroundColor: filters.size > 0 ? theme.colors.primaryContainer : theme.colors.surface }}
+      style={{ backgroundColor: filters.length > 0 ? theme.colors.primaryContainer : theme.colors.surface }}
       {...props}
-      onClose={filters.size > 0 ? props.onClose : undefined}
+      onClose={filters.length > 0 ? props.onClose : undefined}
     >
-      {filters.size > 0 ? getFilterText() : props.children}
+      {filters.length > 0 ? getFilterText() : props.children}
     </Chip>
   )
 }

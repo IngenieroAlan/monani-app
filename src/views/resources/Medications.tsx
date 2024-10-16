78f4ecfa1241ac719bc4723056f650149f5085f1
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-10-15 22:22:37 -0700

    Resources views updated.

--- src/views/resources/Medications.tsx
+++ src/views/resources/Medications.tsx
@@ -1,6 +1,18 @@
+import { useNavigation } from '@react-navigation/native'
+import { View } from 'react-native'
+import { Appbar, useTheme } from 'react-native-paper'
+
 const Medications = () => {
+  const theme = useTheme()
+  const navigation = useNavigation()
+
   return (
-    <></>
+    <View style={{ flex: 1, backgroundColor: theme.colors.surface }}>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Appbar.Content title='Medicamentos' />
+      </Appbar.Header>
+    </View>
   )
 }
 
--- src/views/resources/Resources.tsx
+++ src/views/resources/Resources.tsx
@@ -1,10 +1,32 @@
 import { ResourcesStackParams } from '@/navigation/stacks/ResourcesStack'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
+import { useCallback } from 'react'
 import { View } from 'react-native'
-import { Appbar, useTheme } from 'react-native-paper'
+import { Appbar, Divider, Icon, List, ListItemProps, useTheme } from 'react-native-paper'
 
 type ScreenNavigationProp = NativeStackScreenProps<ResourcesStackParams>
+
+const ListItem = (props: ListItemProps & { iconName: string }) => {
+  const left = useCallback(() => {
+    return (
+      <View style={{ paddingStart: 16 }}>
+        <Icon
+          size={24}
+          source={props.iconName}
+        />
+      </View>
+    )
+  }, [props.iconName])
+
+  return (
+    <List.Item
+      {...props}
+      left={left}
+      right={() => (<Icon size={24} source='menu-right' />)}
+    />
+  )
+}
 
 const Resources = ({ navigation }: ScreenNavigationProp) => {
   const theme = useTheme()
@@ -16,6 +38,17 @@ const Resources = ({ navigation }: ScreenNavigationProp) => {
         <Appbar.BackAction onPress={() => navigation.goBack()} />
         <Appbar.Content title='Recursos' />
       </Appbar.Header>
+      <ListItem
+        title='Alimentos'
+        iconName='grass'
+        onPress={() => navigation.navigate('FeedsView')}
+      />
+      <Divider horizontalInset />
+      <ListItem
+        title='Medicamentos'
+        iconName='needle'
+        onPress={() => navigation.navigate('MedicationsView')}
+      />
     </View>
   )
 }

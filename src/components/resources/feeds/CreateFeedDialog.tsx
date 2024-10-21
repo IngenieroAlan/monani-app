d5498212659ef727b4b5e8a32f077f837f376990
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-10-21 13:34:25 -0700

    DIrectory changed.

--- src/components/resources/feeds/CreateFeedDialog.tsx
+++ src/components/resources/feeds/CreateFeedDialog.tsx
@@ -1,79 +1,68 @@
+import Feed from '@/database/models/Feed'
+import { TableName } from '@/database/schema'
 import { useAppDispatch, useAppSelector } from '@/hooks/useRedux'
-import { setFetchFeeds, setSelectedFeed } from '@/redux/slices/resourcesSlice'
+import { setFetchFeeds } from '@/redux/slices/resourcesSlice'
 import { hide, show } from '@/redux/slices/uiVisibilitySlice'
 import { RootState } from '@/redux/store/store'
 import FeedSchema from '@/validationSchemas/FeedSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useDatabase } from '@nozbe/watermelondb/react'
-import { memo, useCallback, useEffect } from 'react'
+import { memo, useCallback } from 'react'
 import { useForm } from 'react-hook-form'
 import { Keyboard } from 'react-native'
 import { Button, Dialog, Portal } from 'react-native-paper'
 import { z } from 'zod'
-import DismissDialog from '../DismissDialog'
-import FeedForm from '../forms/FeedForm'
+import DismissDialog from '../../DismissDialog'
+import FeedForm from '../../forms/FeedForm'
 import { ResourcesSnackbarId } from './ResourcesSnackbarContainer'
 
-export const EDIT_FEED_DIALOG_ID = 'editFeedDialog'
+export const CREATE_FEED_DIALOG_ID = 'createFeedDialog'
 
+const DISMISS_DIALOG_ID = 'createFeedDismissDialog'
 type FeedFields = z.infer<typeof FeedSchema>
-const DISMISS_DIALOG_ID = 'editFeedDismissDialog'
 
-const EditFeedDialog = () => {
+const CreateFeedDialog = () => {
   const database = useDatabase()
   const dispatch = useAppDispatch()
-  const dialogVisible = useAppSelector((state: RootState) => state.uiVisibility[EDIT_FEED_DIALOG_ID])
-  const selectedFeed = useAppSelector((state: RootState) => state.resources.selectedFeed)
+  const dialogVisible = useAppSelector((state: RootState) => state.uiVisibility[CREATE_FEED_DIALOG_ID])
   const { control, handleSubmit, reset, clearErrors, formState } = useForm<FeedFields>({
     defaultValues: {
-      name: selectedFeed?.name ? selectedFeed.name : '',
-      feedType: selectedFeed?.feedType ? selectedFeed.feedType : undefined
+      name: '',
+      feedType: undefined
     },
     resolver: zodResolver(FeedSchema),
     mode: 'onTouched'
   })
-
   const { isDirty, isValid, isSubmitting } = formState
-
-  useEffect(() => {
-    reset({
-      name: selectedFeed?.name,
-      feedType: selectedFeed?.feedType
-    })
-  }, [selectedFeed])
 
   const dismissChanges = useCallback(() => {
     Keyboard.dismiss()
-    dispatch(setSelectedFeed(undefined))
 
     clearErrors()
     reset()
-    dispatch(hide(EDIT_FEED_DIALOG_ID))
+    dispatch(hide(CREATE_FEED_DIALOG_ID))
   }, [])
 
   const showDismissDialog = useCallback(() => {
-    dispatch(hide(EDIT_FEED_DIALOG_ID))
+    dispatch(hide(CREATE_FEED_DIALOG_ID))
     dispatch(show(DISMISS_DIALOG_ID))
   }, [])
 
-  const onSubmit = useCallback(
-    async (data: FeedFields) => {
-      await database.write(async () => {
-        await selectedFeed?.update((feed) => {
-          feed.name = data.name
-          feed.feedType = data.feedType
-        })
-
-        dispatch(show(ResourcesSnackbarId.UPDATED_FEED))
+  const onSubmit = useCallback(async (data: FeedFields) => {
+    await database.write(async () => {
+      const newFeed = await database.collections.get<Feed>(TableName.FEEDS).create((feed) => {
+        feed.name = data.name
+        feed.feedType = data.feedType
       })
 
-      dismissChanges()
+      dispatch(show(ResourcesSnackbarId.STORED_FEED))
+    })
 
-      // Refresh feeds list.
-      dispatch(setFetchFeeds(true))
-    },
-    [selectedFeed]
-  )
+    dismissChanges()
+
+    // Refresh feeds list.
+    dispatch(setFetchFeeds(true))
+  }, [])
 
   return (
     <Portal>
@@ -82,7 +71,7 @@ const EditFeedDialog = () => {
         visible={dialogVisible}
         onDismiss={dismissChanges}
       >
-        <Dialog.Title>Editar alimento</Dialog.Title>
+        <Dialog.Title>Agregar alimento</Dialog.Title>
         <Dialog.Content>
           <FeedForm
             control={control}
@@ -103,10 +92,10 @@ const EditFeedDialog = () => {
         id={DISMISS_DIALOG_ID}
         snackbarOnDismiss
         onConfirm={dismissChanges}
-        onCancel={() => dispatch(show(EDIT_FEED_DIALOG_ID))}
+        onCancel={() => dispatch(show(CREATE_FEED_DIALOG_ID))}
       />
     </Portal>
   )
 }
 
-export default memo(EditFeedDialog)
+export default memo(CreateFeedDialog)
--- src/components/resources/feeds/EditFeedDialog.tsx
+++ src/components/resources/feeds/EditFeedDialog.tsx
@@ -1,68 +1,79 @@
-import Feed from '@/database/models/Feed'
-import { TableName } from '@/database/schema'
 import { useAppDispatch, useAppSelector } from '@/hooks/useRedux'
-import { setFetchFeeds } from '@/redux/slices/resourcesSlice'
+import { setFetchFeeds, setSelectedFeed } from '@/redux/slices/resourcesSlice'
 import { hide, show } from '@/redux/slices/uiVisibilitySlice'
 import { RootState } from '@/redux/store/store'
 import FeedSchema from '@/validationSchemas/FeedSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useDatabase } from '@nozbe/watermelondb/react'
-import { memo, useCallback } from 'react'
+import { memo, useCallback, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { Keyboard } from 'react-native'
 import { Button, Dialog, Portal } from 'react-native-paper'
 import { z } from 'zod'
-import DismissDialog from '../DismissDialog'
-import FeedForm from '../forms/FeedForm'
+import DismissDialog from '../../DismissDialog'
+import FeedForm from '../../forms/FeedForm'
 import { ResourcesSnackbarId } from './ResourcesSnackbarContainer'
 
-export const CREATE_FEED_DIALOG_ID = 'createFeedDialog'
+export const EDIT_FEED_DIALOG_ID = 'editFeedDialog'
 
-const DISMISS_DIALOG_ID = 'createFeedDismissDialog'
 type FeedFields = z.infer<typeof FeedSchema>
+const DISMISS_DIALOG_ID = 'editFeedDismissDialog'
 
-const CreateFeedDialog = () => {
+const EditFeedDialog = () => {
   const database = useDatabase()
   const dispatch = useAppDispatch()
-  const dialogVisible = useAppSelector((state: RootState) => state.uiVisibility[CREATE_FEED_DIALOG_ID])
+  const dialogVisible = useAppSelector((state: RootState) => state.uiVisibility[EDIT_FEED_DIALOG_ID])
+  const selectedFeed = useAppSelector((state: RootState) => state.resources.selectedFeed)
   const { control, handleSubmit, reset, clearErrors, formState } = useForm<FeedFields>({
     defaultValues: {
-      name: '',
-      feedType: undefined
+      name: selectedFeed?.name ? selectedFeed.name : '',
+      feedType: selectedFeed?.feedType ? selectedFeed.feedType : undefined
     },
     resolver: zodResolver(FeedSchema),
     mode: 'onTouched'
   })
+
   const { isDirty, isValid, isSubmitting } = formState
+
+  useEffect(() => {
+    reset({
+      name: selectedFeed?.name,
+      feedType: selectedFeed?.feedType
+    })
+  }, [selectedFeed])
 
   const dismissChanges = useCallback(() => {
     Keyboard.dismiss()
+    dispatch(setSelectedFeed(undefined))
 
     clearErrors()
     reset()
-    dispatch(hide(CREATE_FEED_DIALOG_ID))
+    dispatch(hide(EDIT_FEED_DIALOG_ID))
   }, [])
 
   const showDismissDialog = useCallback(() => {
-    dispatch(hide(CREATE_FEED_DIALOG_ID))
+    dispatch(hide(EDIT_FEED_DIALOG_ID))
     dispatch(show(DISMISS_DIALOG_ID))
   }, [])
 
-  const onSubmit = useCallback(async (data: FeedFields) => {
-    await database.write(async () => {
-      const newFeed = await database.collections.get<Feed>(TableName.FEEDS).create((feed) => {
-        feed.name = data.name
-        feed.feedType = data.feedType
+  const onSubmit = useCallback(
+    async (data: FeedFields) => {
+      await database.write(async () => {
+        await selectedFeed?.update((feed) => {
+          feed.name = data.name
+          feed.feedType = data.feedType
+        })
+
+        dispatch(show(ResourcesSnackbarId.UPDATED_FEED))
       })
 
-      dispatch(show(ResourcesSnackbarId.STORED_FEED))
-    })
+      dismissChanges()
 
-    dismissChanges()
-
-    // Refresh feeds list.
-    dispatch(setFetchFeeds(true))
-  }, [])
+      // Refresh feeds list.
+      dispatch(setFetchFeeds(true))
+    },
+    [selectedFeed]
+  )
 
   return (
     <Portal>
@@ -71,7 +82,7 @@ const CreateFeedDialog = () => {
         visible={dialogVisible}
         onDismiss={dismissChanges}
       >
-        <Dialog.Title>Agregar alimento</Dialog.Title>
+        <Dialog.Title>Editar alimento</Dialog.Title>
         <Dialog.Content>
           <FeedForm
             control={control}
@@ -92,10 +103,10 @@ const CreateFeedDialog = () => {
         id={DISMISS_DIALOG_ID}
         snackbarOnDismiss
         onConfirm={dismissChanges}
-        onCancel={() => dispatch(show(CREATE_FEED_DIALOG_ID))}
+        onCancel={() => dispatch(show(EDIT_FEED_DIALOG_ID))}
       />
     </Portal>
   )
 }
 
-export default memo(CreateFeedDialog)
+export default memo(EditFeedDialog)

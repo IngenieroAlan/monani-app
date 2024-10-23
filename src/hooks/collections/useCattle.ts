3761f8cee04e433b8e3c96599c02d070b7812726
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-10-23 02:07:01 -0700

    createCattle method implemented.

--- src/hooks/collections/useCattle.ts
+++ src/hooks/collections/useCattle.ts
@@ -1,7 +1,48 @@
-import Cattle from '@/database/models/Cattle'
+import Cattle, { CattleStatus, ProductionType } from '@/database/models/Cattle'
+import Diet, { MatterProportion } from '@/database/models/Diet'
+import DietFeed, { FeedProportion } from '@/database/models/DietFeed'
+import Feed from '@/database/models/Feed'
+import Medication from '@/database/models/Medication'
+import MedicationSchedule from '@/database/models/MedicationSchedule'
 import { TableName } from '@/database/schema'
 import { Q } from '@nozbe/watermelondb'
 import { useDatabase } from '@nozbe/watermelondb/react'
+import { addDays } from 'date-fns'
+
+// TODO: Replace these types by some type infered by zod (z.infer<FormFields>).
+type CattleFields = {
+  name?: string
+  tagId: string
+  tagCattleNumber: string
+  admittedAt: Date
+  bornAt: Date
+  pregnantAt?: Date
+  weight: number
+  quarantineDays?: number
+  productionType: ProductionType
+  cattleStatus: CattleStatus
+}
+
+type DietFields = {
+  waterAmount: number
+  matterAmount?: number
+  percentage?: number
+  matterProportion: MatterProportion
+  isConcentrateExcluded: boolean
+}
+
+type DietFeedFields = {
+  feed: Feed
+  feedAmount: number
+  percentage?: number
+  feedProportion: FeedProportion
+}
+
+type MedicationScheduleFields = {
+  medication: Medication
+  nextDoseAt: Date
+  dosesPerYear: number
+}
 
 const useCattle = () => {
   const database = useDatabase()
@@ -14,10 +55,68 @@ const useCattle = () => {
       .fetch()
   }
 
-  // TODO: create, update, delete, archive, sold, set diet, set medications, set offsprings, set mother, etc.
+  const createCattle = async (
+    cattleData: CattleFields,
+    dietData: DietFields,
+    dietFeedData: DietFeedFields[],
+    medicationScheduleData: MedicationScheduleFields[]
+  ) => {
+    const createdCattle = await database.write(async () => {
+      const diet = await database.collections.get<Diet>(TableName.DIETS)
+        .create((record) => {
+          record.waterAmount = dietData.waterAmount
+          record.matterAmount = dietData.matterAmount
+          record.percentage = dietData.percentage
+          record.matterProportion = dietData.matterProportion
+          record.isConcentrateExcluded = dietData.isConcentrateExcluded
+        })
+      const cattle = await database.collections.get<Cattle>(TableName.CATTLE)
+        .create((record) => {
+          record.diet.set(diet)
+          
+          record.name = cattleData.name
+          record.tagId = cattleData.tagId
+          record.tagCattleNumber = cattleData.tagCattleNumber
+          record.admittedAt = cattleData.admittedAt
+          record.bornAt = cattleData.bornAt
+          record.pregnantAt = cattleData.pregnantAt
+          record.weight = cattleData.weight
+          record.quarantineEndsAt = cattleData.quarantineDays ? addDays(new Date(), cattleData.quarantineDays) : undefined
+          record.productionType = cattleData.productionType
+          record.cattleStatus = cattleData.cattleStatus
+        })
+
+      await database.batch(
+        ...dietFeedData.map((dietFeed) => {
+          return database.collections.get<DietFeed>(TableName.DIET_FEED)
+            .prepareCreate((record) => {
+              record.diet.set(diet)
+              record.feed.set(dietFeed.feed)
+
+              record.feedAmount = dietFeed.feedAmount
+              record.percentage = dietFeed.percentage
+              record.feedProportion = dietFeed.feedProportion
+            })
+        }),
+        ...medicationScheduleData.map((medicationSchedule) => {
+          return database.collections.get<MedicationSchedule>(TableName.MEDICATION_SCHEDULES)
+            .prepareCreate((record) => {
+              record.cattle.set(cattle)
+              record.medication.set(medicationSchedule.medication)
+
+              record.nextDoseAt = medicationSchedule.nextDoseAt
+              record.dosesPerYear = medicationSchedule.dosesPerYear
+            })
+        })
+      )
+    })
+
+    return createdCattle
+  }
 
   return {
-    getCattle
+    getCattle,
+    createCattle
   }
 }
 

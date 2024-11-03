31d284e03b09529daab8478d78548309dcfdfb7e
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-11-03 00:02:46 -0700

    Flashing on content fixed.

--- src/components/ExpandableBottomSheet.tsx
+++ src/components/ExpandableBottomSheet.tsx
@@ -1,38 +1,35 @@
 import useAppTheme from '@/theme'
 import BottomSheet, { BottomSheetProps } from '@gorhom/bottom-sheet'
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { LayoutChangeEvent, View } from 'react-native'
 
 const ExpandableBottomSheet = (props: BottomSheetProps) => {
   const theme = useAppTheme()
 
-  const [initialSnapPoint, setInitialSnapPoint] = useState(1)
-
-  const snapPoints = useMemo(() => [initialSnapPoint, '100%'], [initialSnapPoint])
+  const [snapPoints, setSnapPoints] = useState<(string | number)[]>([])
 
   const handleLayout = useCallback((e: LayoutChangeEvent) => {
-    setInitialSnapPoint(e.nativeEvent.layout.height)
+    setSnapPoints([e.nativeEvent.layout.height, '100%'])
   }, [])
 
-  return (
-    <>
-      <View
-        onLayout={handleLayout}
-        style={{ flex: 1 }}
-      />
-      <BottomSheet
-        backgroundStyle={{ backgroundColor: theme.colors.surface, borderRadius: 0 }}
-        handleIndicatorStyle={{ backgroundColor: theme.colors.outline }}
-        handleStyle={{ paddingVertical: 22 }}
-        enableContentPanningGesture={false}
-        enableOverDrag={false}
-        animateOnMount={false}
-        {...props}
-        snapPoints={snapPoints}
-      >
-        {props.children}
-      </BottomSheet>
-    </>
+  return snapPoints.length === 0 ? (
+    <View
+      onLayout={handleLayout}
+      style={{ flex: 1 }}
+    />
+  ) : (
+    <BottomSheet
+      backgroundStyle={{ backgroundColor: theme.colors.surface, borderRadius: 0 }}
+      handleIndicatorStyle={{ backgroundColor: theme.colors.outline }}
+      handleStyle={{ paddingVertical: 22 }}
+      enableContentPanningGesture={false}
+      enableOverDrag={false}
+      animateOnMount={false}
+      {...props}
+      snapPoints={snapPoints}
+    >
+      {props.children}
+    </BottomSheet>
   )
 }
 

aa69a1dd7c1ccc4afa38cc8ba72b131075b73a2e
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-11-10 01:43:32 -0700

    useNotifications hook added.

---
+++ src/hooks/collections/useNotifications.ts
@@ -0,0 +1,39 @@
+import Notification from '@/database/models/Notification'
+import { TableName } from '@/database/schema'
+import { Q } from '@nozbe/watermelondb'
+import { useDatabase } from '@nozbe/watermelondb/react'
+import { useEffect, useState } from 'react'
+
+type UseNotificationsProps = {
+  isMarkedAsRead?: boolean
+  take?: number
+}
+
+const useNotifications = ({ take, isMarkedAsRead }: UseNotificationsProps = {}) => {
+  const database = useDatabase()
+  const [notifications, setNotifications] = useState<Notification[]>([])
+
+  let notificationsQuery = database.collections
+    .get<Notification>(TableName.NOTIFICATIONS)
+    .query(Q.sortBy('event_at', Q.desc))
+
+  if (isMarkedAsRead !== undefined) {
+    notificationsQuery = notificationsQuery.extend(Q.where('is_marked_as_read', isMarkedAsRead))
+  }
+
+  if (take) {
+    notificationsQuery = notificationsQuery.extend(Q.take(take))
+  }
+
+  useEffect(() => {
+    const subscription = notificationsQuery.observe().subscribe((data) => {
+      setNotifications(data)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [database, isMarkedAsRead, take])
+
+  return { notifications }
+}
+
+export default useNotifications

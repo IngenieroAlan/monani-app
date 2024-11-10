cf7cbd101aee531a8c79d31e5e61013a804efe33
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-11-10 01:41:26 -0700

    Redundat view removed.

--- src/navigation/stacks/NotificationsStack.tsx
+++ src/navigation/stacks/NotificationsStack.tsx
@@ -1,20 +1,20 @@
-
+import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
-import { NotificationsView } from '../../views/notifications/Notifications';
-import { NotificationsDetailsView } from '@/views/notifications/NotificationsDetails';
+import { NotificationsView } from '../../views/notifications/Notifications'
 
 export type NotificationsStackParams = {
-  NotificationsView: undefined;
-  NotificationsDetailsView: undefined;
+  NotificationsView: undefined
+  NotificationsDetailsView: undefined
 }
-const Stack = createStackNavigator<NotificationsStackParams>();
+const Stack = createStackNavigator<NotificationsStackParams>()
 
 export const NotificationsStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='NotificationsView' component={NotificationsView} />
-      <Stack.Screen name='NotificationsDetailsView' component={NotificationsDetailsView} />
+      <Stack.Screen
+        name='NotificationsView'
+        component={NotificationsView}
+      />
     </Stack.Navigator>
   )
 }

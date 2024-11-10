9f5eb0920b5cea90f3462c5b41ebc87abef9967d
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-11-09 20:07:18 -0700

    ForwardRef implemented.

--- src/components/MBottomSheet.tsx
+++ src/components/MBottomSheet.tsx
@@ -1,11 +1,11 @@
 import { close } from '@/redux/slices/bottomSheetSlice'
 import BottomSheet, { BottomSheetBackdrop, BottomSheetProps } from '@gorhom/bottom-sheet'
 import { BottomSheetDefaultBackdropProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types'
-import { useCallback } from 'react'
+import { forwardRef, Ref, useCallback } from 'react'
 import { Portal, useTheme } from 'react-native-paper'
 import { useDispatch } from 'react-redux'
 
-const MBottomSheet = (props: BottomSheetProps & { id?: string }) => {
+const MBottomSheet = (props: BottomSheetProps & { id?: string }, ref?: Ref<BottomSheet>) => {
   const theme = useTheme()
   const dispatch = useDispatch()
 
@@ -22,6 +22,7 @@ const MBottomSheet = (props: BottomSheetProps & { id?: string }) => {
   return (
     <Portal>
       <BottomSheet
+        ref={ref}
         backgroundStyle={{ backgroundColor: theme.colors.elevation.level1 }}
         handleIndicatorStyle={{ backgroundColor: theme.colors.outline }}
         handleStyle={{ paddingVertical: 22 }}
@@ -41,4 +42,4 @@ const MBottomSheet = (props: BottomSheetProps & { id?: string }) => {
   )
 }
 
-export default MBottomSheet
+export default forwardRef(MBottomSheet)

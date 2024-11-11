77b4bf6c96f25a47be781b591b26f0478c3ac2f8
AnguloJuan <jua4_20@alu.uabcs.mx>
2024-11-11 07:15:10 -0700

    Update addCattleSlice to its original value

--- src/redux/slices/addCattleSlice.ts
+++ src/redux/slices/addCattleSlice.ts
@@ -17,7 +17,7 @@ export interface CattleState {
 const initialState: CattleState = {
 	cattle: {
 		name: '',
-		weight: 500,
+		weight: undefined as any,
 		bornAt: undefined,
 		tagId: '',
 		tagCattleNumber: '',

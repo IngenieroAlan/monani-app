061b271f9fcfdcc3335b678c1a6395107ecdaad6
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-11-11 16:33:08 -0700

    Feeds and medications utils created.

---
+++ src/utils/medications.ts
@@ -0,0 +1,17 @@
+import { MedicationFields } from '@/components/forms/MedicationForm'
+import database from '@/database'
+import Medication from '@/database/models/Medication'
+import { TableName } from '@/database/schema'
+
+export const createMedication = async (data: MedicationFields) => {
+  const createdMedication = await database.write(async () => {
+    return await database.collections
+      .get<Medication>(TableName.MEDICATIONS)
+      .create((medication) => {
+        medication.name = data.name
+        medication.medicationType = data.medicationType
+      })
+  })
+
+  return createdMedication
+}

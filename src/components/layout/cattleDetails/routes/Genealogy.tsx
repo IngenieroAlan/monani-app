f78c4b273148dcffe60ad3e579ece7751bc20d58
AnguloJuan <jua4_20@alu.uabcs.mx>
2024-11-18 13:41:25 -0700

    Merged Route with RouteDetails "components"; remove unused code and improve structure

--- src/components/layout/cattleDetails/routes/Genealogy.tsx
+++ src/components/layout/cattleDetails/routes/Genealogy.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
-import { CattleGeneologyDetails } from '../Components/CattleGeneologyDetails'
+import CattleDetailsBottomSheet from "@/components/genealogyRoute/CattleDetailsBottomSheet";
+import GenealogyList from "@/components/genealogyRoute/GenealogyList";
+import GenealogySnackbarContainer from "@/components/genealogyRoute/GenealogySnackbarContainer";
+import { SetMother } from "@/components/genealogyRoute/SetMother";
+import Cattle from "@/database/models/Cattle";
+import { TableName } from "@/database/schema";
 import { useAppSelector } from '@/hooks/useRedux';
+import { withObservables } from "@nozbe/watermelondb/react";
+import React, { useState } from 'react';
+import { View } from "react-native";
+import { useTheme } from "react-native-paper";
 
 export const GenealogyRoute = () => {
   const { cattleInfo } = useAppSelector(state => state.cattles);
@@ -12,3 +20,33 @@ export const GenealogyRoute = () => {
     </>
   )
 }
+
+const observeCattle = withObservables([TableName.CATTLE], ({ cattle }: { cattle: Cattle }) => ({
+  cattle
+}))
+
+const CattleGeneologyDetails = observeCattle(({ cattle }: { cattle: Cattle }) => {
+  const theme = useTheme();
+  const [cattleBottomSheet, setCattleBottomSheet] = useState(-1)
+  const [selectedCattle, setSelectedCattle] = useState<Cattle | null>(null)
+
+  const onSelectedCattle = (cattle: Cattle) => {
+    setSelectedCattle(cattle)
+    setCattleBottomSheet(2)
+  }
+
+  return (<>
+    <View style={{ flex: 1, backgroundColor: theme.colors.surface }}>
+      <SetMother cattle={cattle} onSelectedCattle={onSelectedCattle} />
+      <GenealogyList cattle={cattle} onSelectedCattle={onSelectedCattle} />
+    </View>
+    {selectedCattle && (
+      <CattleDetailsBottomSheet
+        cattleBottomSheet={cattleBottomSheet}
+        setCattleBottomSheet={setCattleBottomSheet}
+        cattle={selectedCattle!}
+      />
+    )}
+    <GenealogySnackbarContainer />
+  </>)
+})
--- src/components/layout/cattleDetails/routes/Weight.tsx
+++ src/components/layout/cattleDetails/routes/Weight.tsx
@@ -1,24 +1,68 @@
+import Cattle from "@/database/models/Cattle";
+import { TableName } from "@/database/schema";
 import { useAppSelector } from '@/hooks/useRedux';
-import { cattleDetails } from '@/styles/main';
-import { format } from 'date-fns';
-import { es } from 'date-fns/locale';
-import React, { useEffect, useState } from 'react'
-import { FlatList, View } from 'react-native'
-import { IconButton, Menu, Text } from 'react-native-paper';
-import { CattleWeightDetails } from '../Components/CattleWeightDetails';
-
-
+import { cattleDetails } from "@/styles/main";
+import { withObservables } from "@nozbe/watermelondb/react";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import React, { useEffect, useState } from 'react';
+import { FlatList, View } from "react-native";
+import { IconButton, Menu, Text } from "react-native-paper";
 
 export const WeightRoute = () => {
-    const { cattleInfo } = useAppSelector(state => state.cattles);
-   return(
-    <>
-    {
-        cattleInfo && <CattleWeightDetails cattle={cattleInfo}/>
-    }
-    </>
-   );
+  const { cattleInfo } = useAppSelector(state => state.cattles);
+ return(
+  <>
+  {
+      cattleInfo && <CattleWeightDetails cattle={cattleInfo}/>
+  }
+  </>
+ );
 }
+
+interface CowWeightReports {
+    weight: number;
+    weightDiference: number;
+    date: Date;
+}
+
+const observeCattle = withObservables([TableName.CATTLE], ({ cattle }: { cattle: Cattle }) => ({
+    cattle
+    //TODO: This should be based on the relation
+}));
+
+const CattleWeightDetails = observeCattle(({ cattle }: { cattle: Cattle }) => {
+    const [weightReports, setWeightReports] = useState<CowWeightReports[]>();
+    useEffect(() => {
+        const fetch = async () => {
+            const weightReportss = await cattle?.weightReports;
+            const AllWeightReports = await Promise.all(
+                weightReportss!.map(async (report) => {
+                    return {
+                        weight: report.weight,
+                        weightDiference: report.weightDifference,
+                        date: report.weighedAt,
+                    };
+                })
+            );
+            setWeightReports(AllWeightReports)
+        };
+        fetch();
+    }, []);
+    return (
+        <FlatList
+            data={weightReports}
+            style={[cattleDetails.container, { paddingLeft: 16, paddingTop: 10, gap: 20 }]}
+            renderItem={({ item }) => (
+                <ActionsMenu
+                    weight={item.weight}
+                    weightDiference={item.weightDiference}
+                    date={item.date}
+                />
+            )}
+        />
+    )
+});
 
 interface ActionProps {
     weight: number;

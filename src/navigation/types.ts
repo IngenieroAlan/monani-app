c260c8a728504a0f6325d24af2c8da35c680e2fb
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-11-20 00:05:42 -0700

    chore: Indentation.

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,82 +1,89 @@
-import { DefaultNavigatorOptions, EventMapBase, NavigationState, NavigatorScreenParams, ParamListBase, RouteConfig, RouteGroupConfig, TabNavigationState } from "@react-navigation/native";
-import { MaterialBottomTabNavigationEventMap, MaterialBottomTabNavigationOptions } from 'react-native-paper';
-import { MaterialBottomTabNavigatorProps } from 'react-native-paper/lib/typescript/react-navigation/navigators/createMaterialBottomTabNavigator';
+import {
+  DefaultNavigatorOptions,
+  EventMapBase,
+  NavigationState,
+  NavigatorScreenParams,
+  ParamListBase,
+  RouteConfig,
+  RouteGroupConfig,
+  TabNavigationState
+} from '@react-navigation/native'
+import { MaterialBottomTabNavigationEventMap, MaterialBottomTabNavigationOptions } from 'react-native-paper'
+import { MaterialBottomTabNavigatorProps } from 'react-native-paper/lib/typescript/react-navigation/navigators/createMaterialBottomTabNavigator'
 
 export type RootStackParamList = {
-  BottomTabsStack: NavigatorScreenParams<BottomTabsParamList>;
-  SearchCattleView: undefined;
-  AddCattleStack: undefined;
-  ResourcesStack: undefined;
+  BottomTabsStack: NavigatorScreenParams<BottomTabsParamList>
+  SearchCattleView: undefined
+  AddCattleStack: undefined
+  ResourcesStack: undefined
   AnnualEarningsView: {
-    year: number;
-    totalEarnings: number;
-    totalCattleEarnings: number;
-    totalMilkEarnings: number;
-    difference: number;
-  };
+    year: number
+    totalEarnings: number
+    totalCattleEarnings: number
+    totalMilkEarnings: number
+    difference: number
+  }
 
-  CattleDetailsLayout: NavigatorScreenParams<CattleInfoParamsList>;
-  DietSettingsRoute: undefined;
-  DietFeedRoute: {
-    dietFeedId: string;
-    modify: boolean;
-  } | undefined;
-  MedicationScheduleRoute: {
-    medicationScheduleId: string;
-    modify: boolean;
-  } | undefined;
-  SearchMotherView: { edit: boolean } | undefined;
-  SearchOffspringView: undefined;
-  ArchiveCattleView: undefined;
-  EditCattleArchiveView: undefined;
-};
+  CattleDetailsLayout: NavigatorScreenParams<CattleInfoParamsList>
+  DietSettingsRoute: undefined
+  DietFeedRoute?: {
+    dietFeedId: string
+    modify: boolean
+  }
+  MedicationScheduleRoute?: {
+    medicationScheduleId: string
+    modify: boolean
+  }
+  SearchMotherView: { edit: boolean } | undefined
+  SearchOffspringView: undefined
+  ArchiveCattleView: undefined
+  EditCattleArchiveView: undefined
+}
 
 export type BottomTabsParamList = {
-  Ganado: undefined;
-  "Prod. lechera": undefined;
-  Ganancias: undefined;
-  Notificaciones: undefined;
-};
+  Ganado: undefined
+  'Prod. lechera': undefined
+  Ganancias: undefined
+  Notificaciones: undefined
+}
 
 export type EarningsStackParamList = {
-  EarningsView: undefined;
-  EarningsResumeView: undefined;
-};
+  EarningsView: undefined
+  EarningsResumeView: undefined
+}
 
 export type ResourcesStackParamList = {
-  ResourcesView: undefined;
-  FeedsView: undefined;
-  MedicationsView: undefined;
-};
+  ResourcesView: undefined
+  FeedsView: undefined
+  MedicationsView: undefined
+}
 
 export type AddCattleStackParamsList = {
-  CattleInfo: undefined;
-  Diet: undefined;
-  DietSettings: undefined;
-  DietFeed: {
-    dietFeedId: string;
-    modify: boolean;
-  } | undefined;
-  Medications: undefined;
-  Medication: {
-    medicationScheduleId: string;
-    modify: boolean;
-  } | undefined;
-};
+  CattleInfo: undefined
+  Diet: undefined
+  DietSettings: undefined
+  DietFeed:
+    | {
+        dietFeedId: string
+        modify: boolean
+      }
+    | undefined
+  Medications: undefined
+  Medication:
+    | {
+        medicationScheduleId: string
+        modify: boolean
+      }
+    | undefined
+}
 export type CattleInfoParamsList = {
-  InfoRoute: undefined;
-  DietRoute: undefined;
-  MedicationRoute: undefined;
-  WeightRoute: undefined;
-  MilkyRoute: undefined;
-  GenealogyRoute: undefined;
-  FirstRoute: undefined;
-};
-
-declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList { }
-  }
+  InfoRoute: undefined
+  DietRoute: undefined
+  MedicationRoute: undefined
+  WeightRoute: undefined
+  MilkyRoute: undefined
+  GenealogyRoute: undefined
+  FirstRoute: undefined
 }
 
 type LegacyTypedNavigator<
@@ -91,13 +98,13 @@ type LegacyTypedNavigator<
       React.ComponentProps<Navigator>,
       keyof DefaultNavigatorOptions<any, any, any, any, any, any>
     > &
-      DefaultNavigatorOptions<ParamList, any, State, ScreenOptions, EventMap, any>
-  >;
-  Group: React.ComponentType<RouteGroupConfig<ParamList, ScreenOptions, any>>;
+    DefaultNavigatorOptions<ParamList, any, State, ScreenOptions, EventMap, any>
+  >
+  Group: React.ComponentType<RouteGroupConfig<ParamList, ScreenOptions, any>>
   Screen: <RouteName extends keyof ParamList>(
     _: RouteConfig<ParamList, RouteName, State, ScreenOptions, EventMap, any>
-  ) => null;
-};
+  ) => null
+}
 
 export type MaterialBottomTabNavigator<T extends ParamListBase> = LegacyTypedNavigator<
   T,
@@ -114,3 +121,9 @@ export type MaterialBottomTabNavigator<T extends ParamListBase> = LegacyTypedNav
     ...rest
   }: MaterialBottomTabNavigatorProps) => React.JSX.Element
 >
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}

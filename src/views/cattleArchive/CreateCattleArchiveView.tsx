1c8f6fc914d700b55efe0c67fa33b129fd1144de
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-11-19 21:15:48 -0700

    Back handling updated.

--- src/views/cattleArchive/CreateCattleArchiveView.tsx
+++ src/views/cattleArchive/CreateCattleArchiveView.tsx
@@ -1,14 +1,52 @@
+import DismissDialog from '@/components/DismissDialog'
 import CattleArchiveForm, { CattleArchiveFields } from '@/components/forms/CattleArchiveForm'
 import { useAppSelector } from '@/hooks/useRedux'
 import useAppTheme from '@/theme'
 import CattleArchiveSchema from '@/validationSchemas/CattleArchiveSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, usePreventRemove } from '@react-navigation/native'
 import { set } from 'date-fns'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { View } from 'react-native'
-import { Appbar, Button, IconButton } from 'react-native-paper'
+import { Appbar, Button, IconButton, Portal } from 'react-native-paper'
+
+const CloseButton = ({ isDirty, isSubmitSuccessful }: { isDirty: boolean; isSubmitSuccessful: boolean }) => {
+  const navigation = useNavigation()
+  const [showDialog, setShowDialog] = useState(false)
+  const confirmGoBack = useRef(false)
+
+  usePreventRemove(isDirty && !isSubmitSuccessful, ({ data }) => {
+    if (!confirmGoBack.current) {
+      setShowDialog(true)
+      return
+    }
+
+    setShowDialog(false)
+    navigation.dispatch(data.action)
+  })
+
+  const onDismissConfirm = useCallback(() => {
+    confirmGoBack.current = true
+    navigation.goBack()
+  }, [])
+
+  return (
+    <>
+      <IconButton
+        icon='close'
+        onPress={navigation.goBack}
+      />
+      <Portal>
+        <DismissDialog
+          visible={showDialog}
+          onConfirm={onDismissConfirm}
+          onCancel={() => setShowDialog(false)}
+        />
+      </Portal>
+    </>
+  )
+}
 
 const CreateCattleArchiveView = () => {
   const theme = useAppTheme()
@@ -24,23 +62,27 @@ const CreateCattleArchiveView = () => {
     mode: 'onChange'
   })
 
-  const { isDirty, isValid, isSubmitting } = formState
+  const { isDirty, isValid, isSubmitting, isSubmitSuccessful } = formState
 
-  useEffect(() => reset(), [])
+  useEffect(() => {
+    if (isSubmitSuccessful) navigation.goBack()
+  }, [isSubmitSuccessful])
 
-  const onSubmit = useCallback(async (data: CattleArchiveFields) => {
-    if (!cattle) return
+  const onSubmit = useCallback(
+    async (data: CattleArchiveFields) => {
+      if (!cattle) return
 
-    await cattle.markAsArchived(data)
-    navigation.goBack()
-  }, [cattle])
+      await cattle.markAsArchived(data)
+    },
+    [cattle]
+  )
 
   return (
     <View style={{ backgroundColor: theme.colors.surface, flex: 1 }}>
       <Appbar.Header>
-        <IconButton
-          icon='close'
-          onPress={navigation.goBack}
+        <CloseButton
+          isDirty={isDirty}
+          isSubmitSuccessful={isSubmitSuccessful}
         />
         <Appbar.Content title='Archivar ganado' />
         <Button

e49582dc6b4ca31f926522a7d663dfcfd381a042
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-11-23 00:13:02 -0700

    Number value handling updated and chore: format.

--- src/components/CustomTextInput.tsx
+++ src/components/CustomTextInput.tsx
@@ -1,43 +1,49 @@
-import React from 'react';
-import { Control, Controller, FieldError, FieldPath, FieldValues } from "react-hook-form";
-import { View, ViewProps } from 'react-native';
-import { HelperText, TextInput, TextInputProps } from 'react-native-paper';
+import { Control, Controller, FieldError, FieldPath, FieldValues } from 'react-hook-form'
+import { View, ViewProps } from 'react-native'
+import { HelperText, TextInput, TextInputProps } from 'react-native-paper'
 
 interface Props<T extends FieldValues> {
-    label: string;
-    helperText: string;
-    placeholder?: string;
-    control: Control<T>;
-    more?: TextInputProps;
-    name: FieldPath<T>;
-    errors: FieldError | undefined;
-    containerStyle?: ViewProps;
+  label: string
+  helperText: string
+  placeholder?: string
+  control: Control<T>
+  more?: TextInputProps
+  name: FieldPath<T>
+  errors: FieldError | undefined
+  containerStyle?: ViewProps
 }
 
-export const CustomTextInput = <T extends FieldValues>({ name, label, placeholder = "", errors, helperText, control, containerStyle, more }: Props<T>) => {
-    return (
-        <View style={containerStyle}>
-            <Controller
-                control={control}
-                render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                        label={label}
-                        value={value}
-                        onBlur={onBlur}
-                        onChangeText={onChange}
-                        placeholder={placeholder}
-                        mode="outlined"
-                        error={errors && true}
-                        {...more}
-                    />)}
-                name={name}
+export const CustomTextInput = <T extends FieldValues>({
+  name,
+  label,
+  placeholder = '',
+  errors,
+  helperText,
+  control,
+  containerStyle,
+  more
+}: Props<T>) => {
+  return (
+    <View style={containerStyle}>
+      <Controller
+        control={control}
+        name={name}
+        render={({ field: { onChange, onBlur, value } }) => {
+          return (
+            <TextInput
+              label={label}
+              value={value && value.toString()}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              placeholder={placeholder}
+              mode='outlined'
+              error={errors && true}
+              {...more}
             />
-            {
-                errors && <HelperText type="error">
-                    {helperText}
-                </HelperText>
-            }
-
-        </View>
-    )
+          )
+        }}
+      />
+      {errors && <HelperText type='error'>{helperText}</HelperText>}
+    </View>
+  )
 }

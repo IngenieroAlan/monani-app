9a32c90a12cf85bec20c054a5e45d54e01fafd39
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-12-16 23:51:20 -0700

    useCattle hook updated.

--- src/hooks/collections/useCattle.ts
+++ src/hooks/collections/useCattle.ts
@@ -1,69 +1,91 @@
-import Cattle, { CattleStatus, ProductionType } from "@/database/models/Cattle";
-import { TableName } from "@/database/schema";
-import { Q } from "@nozbe/watermelondb";
-import { useDatabase } from "@nozbe/watermelondb/react";
-import { useEffect, useState } from "react";
+import Cattle, { CattleStatus, ProductionType } from '@/database/models/Cattle'
+import { TableName } from '@/database/schema'
+import { Q } from '@nozbe/watermelondb'
+import { sanitizeLikeString } from '@nozbe/watermelondb/QueryDescription'
+import { useDatabase } from '@nozbe/watermelondb/react'
+import { useEffect, useState } from 'react'
+import { InteractionManager } from 'react-native'
 
 type UseCattleProps = {
-  cattleStatus?: CattleStatus[] | Set<CattleStatus>;
-  productionType?: ProductionType | null;
-  isActive?: boolean;
-  take?: number;
-};
+  tagId?: string
+  cattleStatus?: CattleStatus[] | Set<CattleStatus>
+  productionType?: ProductionType | null
+  isActive?: boolean
+  isArchived?: boolean
+  isSold?: boolean
+  isInQuarantine?: boolean
+  take?: number
+}
 
 const useCattle = ({
+  tagId,
   cattleStatus,
   productionType,
   isActive,
-  take,
+  isArchived,
+  isSold,
+  isInQuarantine,
+  take
 }: UseCattleProps = {}) => {
-  const database = useDatabase();
-  const [cattleRecords, setCattleRecords] = useState<Cattle[]>([]);
+  const database = useDatabase()
+  const [isPending, setIsPending] = useState(true)
+  const [cattleRecords, setCattleRecords] = useState<Cattle[]>([])
 
-  let cattleQuery = database.collections.get<Cattle>(TableName.CATTLE).query();
+  let cattleQuery = database.get<Cattle>(TableName.CATTLE).query()
 
   if (take) {
-    cattleQuery = cattleQuery.extend(Q.take(take));
+    cattleQuery = cattleQuery.extend(Q.take(take))
   }
 
-  if (cattleStatus && Array.from(cattleStatus).length) {
-    cattleQuery = cattleQuery.extend(
-      Q.where("cattle_status", Q.oneOf(Array.from(cattleStatus)))
-    );
+  if (tagId) {
+    cattleQuery = cattleQuery.extend(Q.where('tag_id', Q.like(`${sanitizeLikeString(tagId)}%`)))
+  }
+
+  if (cattleStatus && Array.from(cattleStatus).length > 0) {
+    cattleQuery = cattleQuery.extend(Q.where('cattle_status', Q.oneOf(Array.from(cattleStatus))))
   }
 
   if (productionType) {
-    cattleQuery = cattleQuery.extend(
-      Q.where("production_type", productionType)
-    );
+    cattleQuery = cattleQuery.extend(Q.where('production_type', productionType))
   }
 
-  if (isActive) {
-    cattleQuery = cattleQuery.extend(
-      Q.where("is_active", isActive)
-    )
+  if (isActive || isArchived || isSold) {
+    const orQuery: Q.Where[] = []
+    isActive && orQuery.push(Q.where('is_active', isActive))
+    isArchived && orQuery.push(Q.where('is_archived', isArchived))
+    isSold && orQuery.push(Q.where('is_sold', isSold))
+
+    cattleQuery = cattleQuery.extend(Q.or(orQuery))
+  }
+
+  if (isInQuarantine !== undefined) {
+    cattleQuery = cattleQuery.extend(Q.where('quarantine_ends_at', isInQuarantine ? Q.notEq(null) : Q.eq(null)))
   }
 
   useEffect(() => {
+    setIsPending(true)
+
     const subscription = cattleQuery.observe().subscribe((data) => {
-      setCattleRecords(data);
-    });
+      InteractionManager.runAfterInteractions(() => {
+        setCattleRecords(data)
+      }).then(() => setIsPending(false))
+    })
 
-    return () => subscription.unsubscribe();
-  }, [database, cattleStatus, productionType, isActive, take]);
+    return () => subscription.unsubscribe()
+  }, [tagId, cattleStatus, productionType, isActive, isArchived, isSold, isInQuarantine])
 
-  const getCattle = async (extend: Q.Clause[]) => {
-    return await database.collections
-      .get<Cattle>(TableName.CATTLE)
-      .query()
-      .extend(extend)
-      .fetch();
-  };
+  // runAfterInteractions seems to work fine for now, but it shouldn't be used here.
+  useEffect(() => {
+    const subscription = cattleQuery.observe().subscribe((data) => {
+      InteractionManager.runAfterInteractions(() => {
+        setCattleRecords(data)
+      })
+    })
 
-  return {
-    cattleRecords,
-    getCattle,
-  };
-};
+    return () => subscription.unsubscribe()
+  }, [take])
 
-export default useCattle;
+  return { cattleRecords, isPending }
+}
+
+export default useCattle

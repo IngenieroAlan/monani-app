679ad11dd5e87c31ad17622da067be5275752ead
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-12-29 00:01:58 -0700

    useFeeds: isPending state implemented.

--- src/hooks/collections/useFeeds.ts
+++ src/hooks/collections/useFeeds.ts
@@ -3,26 +3,42 @@ import { TableName } from '@/database/schema'
 import { Q } from '@nozbe/watermelondb'
 import { useDatabase } from '@nozbe/watermelondb/react'
 import { useEffect, useState } from 'react'
+import { InteractionManager } from 'react-native'
 
-const useFeeds = () => {
+type useFeedsProps = {
+  take?: number
+}
+
+const useFeeds = ({ take }: useFeedsProps = {}) => {
   const database = useDatabase()
-  const [feeds, setFeeds] = useState<Feed[]>([])
+  const [feedsRecords, setFeedsRecords] = useState<Feed[]>([])
+  const [isPending, setIsPending] = useState(true)
 
-  let feedsQuery = database.collections
-    .get<Feed>(TableName.FEEDS)
-    .query(
-      Q.sortBy('name', Q.asc)
-    )
+  let feedsQuery = database.collections.get<Feed>(TableName.FEEDS).query(Q.sortBy('name', Q.asc))
 
   useEffect(() => {
+    setIsPending(true)
+
     const subscription = feedsQuery.observe().subscribe((data) => {
-      setFeeds(data)
+      InteractionManager.runAfterInteractions(() => {
+        setFeedsRecords(data)
+      }).then(() => setIsPending(false))
     })
 
     return () => subscription.unsubscribe()
-  }, [database])
+  }, [])
 
-  return { feeds }
+  useEffect(() => {
+    const subscription = feedsQuery.observe().subscribe((data) => {
+      InteractionManager.runAfterInteractions(() => {
+        setFeedsRecords(data)
+      })
+    })
+
+    return () => subscription.unsubscribe()
+  }, [take])
+
+  return { feedsRecords, isPending }
 }
 
 export default useFeeds

84623adf628e463cd1fe3fa49f09f9b1e97be963
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2024-12-29 00:38:01 -0700

    NotificationsView: SurfaceContainer component updated.

--- src/views/notifications/NotificationsView.tsx
+++ src/views/notifications/NotificationsView.tsx
@@ -1,11 +1,12 @@
 import { SurfaceContainer } from '@/components/SurfaceContainer'
 import NotificationsAppbar from '@/views/notifications/components/NotificationsAppbar'
 import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { NotificationsList } from './components/NotificationsList/NotificationsList'
 
 const NotificationsView = () => {
   return (
-    <SurfaceContainer>
+    <SurfaceContainer Component={SafeAreaView}>
       <NotificationsAppbar />
       <StatusBar />
       <NotificationsList />

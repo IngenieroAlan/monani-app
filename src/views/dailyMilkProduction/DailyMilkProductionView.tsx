cec1c36054175127f1d9abc4e79dce96086b9536
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2025-01-13 17:24:52 -0700

    Added provider.

--- src/views/dailyMilkProduction/DailyMilkProductionView.tsx
+++ src/views/dailyMilkProduction/DailyMilkProductionView.tsx
@@ -1,10 +1,13 @@
 import { SurfaceContainer } from '@/components/SurfaceContainer'
+import { MilkProductionsFiltersProvider } from '@/contexts/MilkProductionsFiltersContext'
 import { MilkProductionsList } from './components/MilkProductionsList'
 
 export const DailyMilkProductionView = () => {
   return (
-    <SurfaceContainer>
-      <MilkProductionsList />
-    </SurfaceContainer>
+    <MilkProductionsFiltersProvider>
+      <SurfaceContainer>
+        <MilkProductionsList />
+      </SurfaceContainer>
+    </MilkProductionsFiltersProvider>
   )
 }

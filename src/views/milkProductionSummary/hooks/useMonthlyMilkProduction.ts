05a0b22fe00100b8d5665b8334f300ca378ba8fc
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2025-01-31 15:36:03 -0700

    Records grouped by year.

--- src/views/milkProductionSummary/hooks/useMonthlyMilkProduction.ts
+++ src/views/milkProductionSummary/hooks/useMonthlyMilkProduction.ts
@@ -1,24 +1,42 @@
-import { MonthlyMilkProductionsCol as Column, TableName } from '@/database/constants'
+import { TableName } from '@/database/constants'
 import MonthlyMilkProduction from '@/database/models/MonthlyMilkProduction'
 import { useDatabase } from '@nozbe/watermelondb/react'
-import { useEffect, useState } from 'react'
+import { map } from '@nozbe/watermelondb/utils/rx'
+import { useEffect, useRef, useState } from 'react'
 import { InteractionManager } from 'react-native'
 
-export const useMonthlyMilkProduction = () => {
-  const database = useDatabase()
-  const [records, setRecords] = useState<MonthlyMilkProduction[]>([])
-  const [isPending, setIsPending] = useState(true)
+const groupByYear = (data: MonthlyMilkProduction[]) => {
+  return Object.entries(
+    data.reduce<Record<number, (MonthlyMilkProduction | null)[]>>((acc, monthlyProduction) => {
+      const year = monthlyProduction.year
 
-  const query = database.get<MonthlyMilkProduction>(TableName.MONTHLY_MILK_PRODUCTIONS).query()
+      if (!acc[year]) {
+        acc[year] = new Array<MonthlyMilkProduction | null>(12).fill(null)
+      }
+      acc[year][11 - monthlyProduction.month] = monthlyProduction
+
+      return acc
+    }, {})
+  )
+}
+
+export const useMonthlyMilkProduction = () => {
+  const db = useDatabase()
+  const [records, setRecords] = useState<[string, (MonthlyMilkProduction | null)[]][]>([])
+  const [isPending, setIsPending] = useState(true)
+  const { current: query } = useRef(db.get<MonthlyMilkProduction>(TableName.MONTHLY_MILK_PRODUCTIONS).query())
 
   useEffect(() => {
     setIsPending(true)
 
-    const subscription = query.observeWithColumns([Column.LITERS]).subscribe((data) => {
-      InteractionManager.runAfterInteractions(() => {
-        setRecords(data)
-      }).then(() => setIsPending(false))
-    })
+    const subscription = query
+      .observe()
+      .pipe(map(groupByYear))
+      .subscribe((data) => {
+        InteractionManager.runAfterInteractions(() => {
+          setRecords(data.toReversed())
+        }).then(() => setIsPending(false))
+      })
 
     return () => subscription.unsubscribe()
   }, [])

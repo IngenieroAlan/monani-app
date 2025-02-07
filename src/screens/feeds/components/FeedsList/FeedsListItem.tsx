5643a39cd7c11d1011713379adefd535cced3589
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2025-02-06 20:10:52 -0700

    Reverted: Feed and medication item caching.

--- src/screens/feeds/components/FeedsList/FeedsListItem.tsx
+++ src/screens/feeds/components/FeedsList/FeedsListItem.tsx
@@ -1,20 +1,11 @@
 import Feed from '@/database/models/Feed'
-import { feedsKeys } from '@/queries/feeds/queryKeyFactory'
 import { withObservables } from '@nozbe/watermelondb/react'
-import { useQueryClient } from '@tanstack/react-query'
-import { useEffect } from 'react'
 import { List } from 'react-native-paper'
 import { FeedsListItemMenu } from './FeedsListItemMenu'
 
 const withObserver = withObservables(['feed'], ({ feed }: { feed: Feed }) => ({ feed }))
 
 export const FeedsListItem = withObserver(({ feed }: { feed: Feed }) => {
-  const queryClient = useQueryClient()
-
-  useEffect(() => {
-    queryClient.setQueryData<Feed>(feedsKeys.byId(feed.id), feed)
-  }, [feed.id])
-
   return (
     <List.Item
       title={feed.name}
--- src/screens/medications/components/MedicationsList/MedicationsListItem.tsx
+++ src/screens/medications/components/MedicationsList/MedicationsListItem.tsx
@@ -1,20 +1,11 @@
 import Medication from '@/database/models/Medication'
-import { medicationsKeys } from '@/queries/medications/queryKeyFactory'
 import { withObservables } from '@nozbe/watermelondb/react'
-import { useQueryClient } from '@tanstack/react-query'
-import { useEffect } from 'react'
 import { List } from 'react-native-paper'
 import { MedicationsListItemMenu } from './MedicationsListItemMenu'
 
 const withObserver = withObservables(['medication'], ({ medication }: { medication: Medication }) => ({ medication }))
 
 export const MedicationsListItem = withObserver(({ medication }: { medication: Medication }) => {
-  const queryClient = useQueryClient()
-
-  useEffect(() => {
-    queryClient.setQueryData<Medication>(medicationsKeys.byId(medication.id), medication)
-  }, [medication.id])
-
   return (
     <List.Item
       style={{ paddingRight: 4 }}

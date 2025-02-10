5084f6cdc5db1e103605858b75d0b24e9ee4f1d9
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2025-02-10 14:35:48 -0700

    DateDisplay component added.

--- src/screens/milkProduction/dailyMilkProduction/components/MilkProductionsListItem.tsx
+++ src/screens/milkProduction/dailyMilkProduction/components/MilkProductionsListItem.tsx
@@ -1,50 +1,82 @@
 import DailyMilkProduction from '@/database/models/DailyMilkProduction'
+import useAppTheme from '@/theme'
 import { formatNumberWithSpaces } from '@/utils/helpers'
 import { withObservables } from '@nozbe/watermelondb/react'
 import { useNavigation } from '@react-navigation/native'
-import { format } from 'date-fns'
+import dayjs from 'dayjs'
 import { memo } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Icon, List, Text } from 'react-native-paper'
 
-const Right = memo(({ date }: { date: string }) => {
+const DateDisplay = memo(({ date }: { date: Date }) => {
   return (
-    <View style={styles.rightContainer}>
-      <Text variant='labelSmall'>{date}</Text>
-      <Icon
-        size={24}
-        source='chevron-right'
-      />
+    <View style={styles.dateContainer}>
+      <Text variant='labelLarge'>{dayjs(date).format('DD')}</Text>
+      <Text
+        variant='labelSmall'
+        adjustsFontSizeToFit
+        numberOfLines={1}
+      >
+        {dayjs(date).format(`MMM.${dayjs().isSame(date, 'year') ? '' : ' YY'}`)}
+      </Text>
     </View>
   )
 })
 
 const withObserver = withObservables(
   ['milkProduction'],
-  ({ milkProduction }: { milkProduction: DailyMilkProduction }) => ({
-    milkProduction
+  ({ milkProduction }: { milkProduction: DailyMilkProduction }) => ({ milkProduction })
+)
+
+export const MilkProductionsListItem = memo(
+  withObserver(({ milkProduction }: { milkProduction: DailyMilkProduction }) => {
+    const theme = useAppTheme()
+    const navigation = useNavigation()
+
+    return (
+      <View style={styles.container}>
+        <DateDisplay date={milkProduction.producedAt} />
+        <List.Item
+          style={[{ borderColor: theme.colors.outlineVariant, flex: 1 }, styles.listItem]}
+          title={<Text variant='titleMedium'>{formatNumberWithSpaces(milkProduction.liters.toFixed(3))} L.</Text>}
+          borderless
+          description={() => (
+            <Text
+              variant='labelSmall'
+              style={{ color: theme.colors.onSurfaceVariant, opacity: 0.7 }}
+            >
+              {milkProduction.totalProductions} {milkProduction.totalProductions === 1 ? 'producción' : 'producciones'}
+            </Text>
+          )}
+          right={({ style }) => (
+            <View style={style}>
+              <Icon
+                size={24}
+                source='chevron-right'
+              />
+            </View>
+          )}
+          onPress={() => navigation.navigate('DailyProductionDetailsView')}
+        />
+      </View>
+    )
   })
 )
 
-export const MilkProductionsListItem = withObserver(({ milkProduction }: { milkProduction: DailyMilkProduction }) => {
-  const navigation = useNavigation()
-
-  return (
-    <List.Item
-      title={`${formatNumberWithSpaces(milkProduction.liters.toFixed(3))} L.`}
-      description={`${milkProduction.totalProductions} ${
-        milkProduction.totalProductions === 1 ? 'producción' : 'producciones'
-      }`}
-      right={() => <Right date={format(milkProduction.producedAt, 'dd/MM/yyyy')} />}
-      onPress={() => navigation.navigate('DailyProductionDetailsView')}
-    />
-  )
-})
-
 const styles = StyleSheet.create({
-  rightContainer: {
+  container: {
     flexDirection: 'row',
+    marginHorizontal: 16,
+    gap: 12
+  },
+  listItem: {
+    borderWidth: 0.5,
+    borderRadius: 12,
+    paddingRight: 16
+  },
+  dateContainer: {
     alignItems: 'center',
-    gap: 8
+    paddingTop: 4,
+    width: 52
   }
 })

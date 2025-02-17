a3e74c36c768c9216e93ea504d63c42ba4573591
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2025-02-17 15:11:50 -0700

    Milk production queryKeyFactory added.

--- src/queries/milkProduction/queryKeyFactory.ts
+++ src/queries/milkProduction/queryKeyFactory.ts
@@ -1,8 +1,19 @@
+import dayjs from 'dayjs'
 import { DailyMilkProductionsFilters } from './useInfiniteDailyMilkProductionsQuery'
 
 export const milkProductionsKeys = {
   all: ['milkProductions'] as const,
   byId: (id: string) => [...milkProductionsKeys.all, { id }] as const,
   daily: () => [...milkProductionsKeys.all, 'daily'] as const,
-  dailyFiltered: (filters: DailyMilkProductionsFilters) => [...milkProductionsKeys.daily(), filters] as const
+  dailyFiltered: (filters: DailyMilkProductionsFilters) => [...milkProductionsKeys.daily(), filters] as const,
+  groupedByDate: (date: dayjs.ConfigType) => {
+    return [...milkProductionsKeys.daily(), dayjs(date).hour(0).minute(0).second(0).millisecond(0).valueOf()] as const
+  }
+}
+
+export const milkReportsKeys = {
+  all: ['milkReports'],
+  groupedByProduction: (productionId: string) => {
+    return [...milkReportsKeys.all, { productionId }] as const
+  }
 }

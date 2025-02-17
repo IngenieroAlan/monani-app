ddd74fcdcb8aa2592896c8dc5a288cdc097caa63
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2025-02-17 15:00:23 -0700

    chore: Refactoring.

--- src/components/ExpandableBottomSheet.tsx
+++ src/components/ExpandableBottomSheet.tsx
@@ -1,23 +1,22 @@
 import useAppTheme from '@/theme'
 import BottomSheet, { BottomSheetProps } from '@gorhom/bottom-sheet'
-import { useCallback, useState } from 'react'
-import { LayoutChangeEvent, View } from 'react-native'
+import { useState } from 'react'
+import { View } from 'react-native'
 
 const ExpandableBottomSheet = (props: BottomSheetProps) => {
   const theme = useAppTheme()
-
   const [snapPoints, setSnapPoints] = useState<(string | number)[]>([])
 
-  const handleLayout = useCallback((e: LayoutChangeEvent) => {
-    setSnapPoints([e.nativeEvent.layout.height, '100%'])
-  }, [])
+  if (snapPoints.length === 0) {
+    return (
+      <View
+        onLayout={(e) => setSnapPoints([e.nativeEvent.layout.height, '100%'])}
+        style={{ flex: 1 }}
+      />
+    )
+  }
 
-  return snapPoints.length === 0 ? (
-    <View
-      onLayout={handleLayout}
-      style={{ flex: 1 }}
-    />
-  ) : (
+  return (
     <BottomSheet
       backgroundStyle={{ backgroundColor: theme.colors.surface, borderRadius: 0 }}
       handleIndicatorStyle={{ backgroundColor: theme.colors.outline }}

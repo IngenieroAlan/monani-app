6ab9a4f6b1f4d268429ed237d13c536d8fe60ff9
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2025-02-18 23:34:54 -0700

    DailyInfoCard component implemented.

--- src/screens/milkProduction/dailyProductionDetails/DailyProductionDetailsView.tsx
+++ src/screens/milkProduction/dailyProductionDetails/DailyProductionDetailsView.tsx
@@ -1,10 +1,31 @@
 import { SurfaceContainer } from '@/components/SurfaceContainer'
-import { Text } from 'react-native-paper'
+import { MainStackParamList } from '@/navigation/types'
+import { formatDateRelativeToYear } from '@/utils/helpers'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { View } from 'react-native'
+import { Appbar, Divider } from 'react-native-paper'
+import { DailyInfoCard } from './components/DailyInfoCard'
 
-export const DailyProductionDetailsView = () => {
+type ScreenProps = NativeStackScreenProps<MainStackParamList, 'DailyProductionDetailsView'>
+
+export const DailyProductionDetailsView = ({ route, navigation }: ScreenProps) => {
+  const { productionTimestamp, totalLiters } = route.params
+
   return (
     <SurfaceContainer>
-      <Text>DailyProductionDetails</Text>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={navigation.goBack} />
+        <Appbar.Content title={formatDateRelativeToYear(productionTimestamp)} />
+      </Appbar.Header>
+      <View style={{ flex: 1 }}>
+        <View style={{ padding: 16 }}>
+          <DailyInfoCard
+            totalLiters={totalLiters}
+            productionTimestamp={productionTimestamp}
+          />
+        </View>
+        <Divider />
+      </View>
     </SurfaceContainer>
   )
 }
---
+++ src/screens/milkProduction/dailyProductionDetails/components/DailyInfoCard.tsx
@@ -0,0 +1,61 @@
+import NumericDifference from '@/components/NumericDifference'
+import { DailyMilkProductionsCol, TableName } from '@/database/constants'
+import DailyMilkProduction from '@/database/models/DailyMilkProduction'
+import { milkProductionsKeys } from '@/queries/milkProduction/queryKeyFactory'
+import useAppTheme from '@/theme'
+import { formatNumberWithSpaces } from '@/utils/helpers'
+import { Q } from '@nozbe/watermelondb'
+import { useDatabase } from '@nozbe/watermelondb/react'
+import { useQuery } from '@tanstack/react-query'
+import { View } from 'react-native'
+import { Card, Text } from 'react-native-paper'
+
+type Props = {
+  totalLiters: number
+  productionTimestamp: number
+}
+
+export const DailyInfoCard = ({ totalLiters, productionTimestamp }: Props) => {
+  const db = useDatabase()
+  const theme = useAppTheme()
+
+  const { data, isLoading } = useQuery({
+    queryKey: milkProductionsKeys.dayBefore(productionTimestamp),
+    queryFn: () =>
+      db
+        .get<DailyMilkProduction>(TableName.DAILY_MILK_PRODUCTIONS)
+        .query(
+          Q.where(DailyMilkProductionsCol.PRODUCED_AT, Q.lt(productionTimestamp)),
+          Q.sortBy(DailyMilkProductionsCol.PRODUCED_AT, Q.desc),
+          Q.take(1)
+        )
+        .fetch()
+  })
+
+  return (
+    <Card mode='outlined'>
+      <Card.Content style={{ gap: 12 }}>
+        <View style={{ gap: 4 }}>
+          <View>
+            <Text
+              variant='labelMedium'
+              style={{ color: theme.colors.onSurfaceVariant, opacity: 0.7 }}
+            >
+              Total de litros producidos
+            </Text>
+            <Text variant='displayMedium'>{formatNumberWithSpaces(totalLiters.toFixed(3))} L.</Text>
+          </View>
+          {isLoading && <Text>Cargando...</Text>}
+          {!isLoading && data && data?.length > 0 && (
+            <NumericDifference
+              variant='labelMedium'
+              difference={totalLiters - data[0].liters}
+              fractionDigits={3}
+              suffix=' L. comparado a la producción anterior'
+            />
+          )}
+        </View>
+      </Card.Content>
+    </Card>
+  )
+}

2146815997efcfc8d5aa65d15a0889ece4da4ed4
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2025-02-18 23:32:22 -0700

    dayBefore key added.

--- src/queries/milkProduction/queryKeyFactory.ts
+++ src/queries/milkProduction/queryKeyFactory.ts
@@ -4,10 +4,15 @@ import { DailyMilkProductionsFilters } from './useInfiniteDailyMilkProductionsQu
 export const milkProductionsKeys = {
   all: ['milkProductions'] as const,
   byId: (id: string) => [...milkProductionsKeys.all, { id }] as const,
+
   daily: () => [...milkProductionsKeys.all, 'daily'] as const,
+  dayBefore: (date: dayjs.ConfigType) => [...milkProductionsKeys.daily(), { before: dayjs(date).valueOf() }] as const,
   dailyFiltered: (filters: DailyMilkProductionsFilters) => [...milkProductionsKeys.daily(), filters] as const,
   groupedByDate: (date: dayjs.ConfigType) => {
-    return [...milkProductionsKeys.daily(), dayjs(date).hour(0).minute(0).second(0).millisecond(0).valueOf()] as const
+    return [
+      ...milkProductionsKeys.daily(),
+      { date: dayjs(date).hour(0).minute(0).second(0).millisecond(0).valueOf() }
+    ] as const
   }
 }
 
